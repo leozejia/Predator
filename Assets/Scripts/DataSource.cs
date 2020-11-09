51c3c3eb597ab9d33856151000d52e4e75e77a5f
leozejia <44258909+leozejia@users.noreply.github.com>
2020-11-09 21:10:58 +0800

    界面调整，场景调整，dll测试

---
+++ Assets/Scripts/DataSource.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class DataSource : MonoBehaviour
+{
+    [DllImport("testdllsarjam")]
+    private static extern int funcjammer(double[] x, int y);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        double[] x = { 112.66611111, 35.19555556, 6000, 0.0 };
+        //Debug.Log(funcjammer(x, 4));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
