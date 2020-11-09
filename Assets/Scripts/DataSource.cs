c2f71afbf81fbf0b112d3561addef05c4aa49267
leozejia <44258909+leozejia@users.noreply.github.com>
2020-11-09 21:44:39 +0800

    TESTDll

--- Assets/Scripts/DataSource.cs
+++ Assets/Scripts/DataSource.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
+using System;
 
 public class DataSource : MonoBehaviour
 {
     [DllImport("testdllsarjam")]
-    private static extern int funcjammer(double[] x, int y);
+    private static extern int funcjammer(double[] x,int y);
 
     // Start is called before the first frame update
     void Start()
     {
-        double[] x = { 112.66611111, 35.19555556, 6000, 0.0 };
-        //Debug.Log(funcjammer(x, 4));
+        double[] sarsingle = new double[4];
+        sarsingle[0] = 112.66611111; //经度
+        sarsingle[1] = 35.19555556; //纬度
+        sarsingle[2] = 6000; //高度
+        sarsingle[3] = 0.0; //航向（北偏东）
+        try
+        {
+            Debug.Log(funcjammer(sarsingle, 4));
+        }
+        catch (Exception e){
+            Debug.LogError(e.Message);
+        }
     }
 
     // Update is called once per frame
